d74d714138493c648f61224b04e603ca096dfbeb
Leon Tellechea <leontelletxea@MacBook-Pro-de-Leon.local>
2025-09-28 16:55:38 -0300

    WhatsAppButton component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import NavBar from './components/NavBar'
 import HeroSection from './components/HeroSection'
 import About from './components/About'
 import Footer from './components/Footer'
+import WhatsAppButton from './components/WhatsAppButton'
 
 function App() {
   return (
@@ -10,6 +11,10 @@ function App() {
       <HeroSection />
       <About />
       <Footer />
+      <WhatsAppButton 
+        phoneNumber="+5491160562814" 
+        message="¡Hola! Me interesa obtener más información sobre sus servicios." 
+      />
     </>
   )
 }
